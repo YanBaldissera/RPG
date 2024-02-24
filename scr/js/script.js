4c1f536b46c2c6b74d7c8c2d332b320c40fcda81
Yan <yanbaldissera1@gmail.com>
2024-02-24 17:06:39 -0300

    atualização responsivo 2

--- scr/js/script.js
+++ scr/js/script.js
@@ -23,7 +23,7 @@ function updatePersonagens(profilePersonagens){
                 </div>
                 <div class="post-content">
                     <div class="titulo">
-                    <img src="${person.icone}" alt="Icone da classe"><h1 class="nome-classe">${person.Classe}</h1>
+                    <img class="foto-icone" src="${person.icone}" alt="Icone da classe"><h1 class="nome-classe">${person.Classe}</h1>
                     </div>
 
                     <div class="titulo2">
